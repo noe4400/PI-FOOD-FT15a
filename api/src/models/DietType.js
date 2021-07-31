c371b3f2f74b9e3d637e29dd676eb7cd603720e3
Fabian Garcia <gfabian757@gmail.com>
2021-07-31 16:51:18 -0500

    Diet type model was created and composite key for recipe model was modified

--- api/src/models/DietType.js
+++ api/src/models/DietType.js
@@ -2,10 +2,6 @@ const { DataTypes, DATE } = require('sequelize');
 
 module.exports = sequealize => {
 	sequealize.define('type', {
-		id: {
-			type: DataTypes.INT,
-			allowNull: false,
-		},
 		name: {
 			type: DataTypes.STRING,
 			allowNull: false,
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,31 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+module.exports = sequelize => {
+	// defino el modelo
+	sequelize.define('recipe', {
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			primaryKey: true,
+		},
+		summary: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		score: {
+			type: DataTypes.DOUBLE,
+		},
+		healthscore: {
+			type: DataTypes.DOUBLE,
+		},
+		steps: {
+			type: DataTypes.STRING,
+		},
+	});
 };
