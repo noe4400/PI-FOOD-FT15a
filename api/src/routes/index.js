156d9655c5d8c9136fa39ee9ed00b7f83ddd870c
Fabian Garcia <gfabian757@gmail.com>
2021-07-31 17:24:51 -0500

    adding post route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
+const { Recipe } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.post('/recipe', async (req, res) => {
+	const { name, summary, score, healthscore, steps } = req.body;
+	const newRecipe = await Recipe.create({
+		name,
+		summary,
+		score,
+		healthscore,
+		steps,
+	});
+	res.send('Recipe was created successfully');
+});
 
 module.exports = router;
