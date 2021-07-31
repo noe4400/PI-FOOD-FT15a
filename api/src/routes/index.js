476ebe41613191ebda2677562a76c622f5b1bc1c
Fabian Garcia <gfabian757@gmail.com>
2021-07-31 18:27:12 -0500

    adding get routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { Recipe } = require('../db');
+const { Op } = require('sequelize');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,25 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get('/recipes/:id', async (req, res) => {
+	const { id } = req.params;
+	if (!id) return res.status(404).send('invalid id');
+	const getRecipeById = await Recipe.findByPk(id);
+	if (!getRecipe) return res.status(404).send('Not found');
+	res.json.send(getRecipeById);
+});
+
+router.get('/recipes', async (req, res) => {
+	const { name } = req.query;
+	if (!name) return res.status(404).send('Invalid name');
+	const getRecipe = await Recipe.findAll({
+		where: { name: { [Op.iLike]: `%${name}%` } },
+	});
+
+	if (!getRecipe) return res.status(404).send('Not found');
+	res.status(202).send(getRecipe);
+});
+
 router.post('/recipe', async (req, res) => {
 	const { name, summary, score, healthscore, steps } = req.body;
 	const newRecipe = await Recipe.create({
